16a52af137145e058584915f1a1cc4307c41b341
LucyQ19 <lucyq19@gmail.com>
2019-04-09 18:05:57 -0500

    update app.js to display music events

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -50,8 +50,6 @@ $(document).ready(function(){
             artists.push(results[i]._embedded.attractions[0].name); 
           }
          
-
-
             // TODO: FIND ALL ARTISTS
             var artistRequests=[];
 
@@ -91,7 +89,38 @@ $(document).ready(function(){
               });
             })
             .catch(console.log);
-        })
+        });
+
+      function displayMusicEvents() {
+        
+        var musicDiv = $("<div>");
+        musicDiv.addClass("card-image waves-effect waves-block waves-light");
+
+        var imgURL = results[0].images[0];
+        var image = $("<img>").attr("src", imgURL);
+        musicDiv.append(image);
+        console.log(image);
+
+        var artistName = results[0].names;
+        var name = $("<p>").text("Name: " + artistName);
+        musicDiv.append(name);
+        console.log(name);
+
+        var dateTime = results[0].dates.start[0].localDate.localTime;
+        var date = $("<p>").text("Date and Time: " + dateTime);
+        musicDiv.append(date);
+        console.log(date);
+
+        var ticketURL = results[0].ticketLimit.url;
+        var ticket = $("<p>").text(ticketURL);
+        musicDiv.append(ticket);
+        console.log(ticket);
+
+
+    };
+
+    $(document).on(displayMusicEvents);
+
 
 
 
