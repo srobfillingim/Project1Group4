b4acef3817a1ab0512c5391ac27bf0db2b21af66
LucyQ19 <lucyq19@gmail.com>
2019-04-11 18:58:11 -0500

    updated displayMusicEvents

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -39,16 +39,26 @@ $(document).ready(function(){
   //     });
 
   var bands = {};
-
+ //api1 =
       getEvents()
         .then(events=>{
-          console.log(events)
           var results = events._embedded.events;
-
+        
           var artists = [];
+
           for(var i = 0; i < results.length; i++){
-            artists.push(results[i]._embedded.attractions[0].name); 
+            var eventObject = {};
+            eventObject.name = results[i]._embedded.attractions[0].name;
+            eventObject.date = results[i].dates.start.localDate;
+            eventObject.time = results[i].dates.start.localTime;
+            eventObject.venuename = results[i]._embedded.venues[0].name;
+            eventObject.ticketurl = results[i].url;
+            eventObject.image = results[i].images[2].url;
+
+            artists.push(eventObject);
           }
+
+          displayMusicEvents(artists);
          
             // TODO: FIND ALL ARTISTS
             var artistRequests=[];
@@ -56,10 +66,10 @@ $(document).ready(function(){
             // TODO: forEach Artist
             // forEach
             artists.forEach(artist=>{
-              console.log(artist);
-              artistRequests.push(getArtist(artist));
+              //console.log(artists);
+              artistRequests.push(getArtist(artist.name));
             })
-            console.log(artistRequests);
+            //console.log(artistRequests);
 
             var verified = [];
             Promise.all(artistRequests)
@@ -79,54 +89,124 @@ $(document).ready(function(){
               // forEach
               var artistEventsRequests = [];
               verified.forEach(artist=>{
-                console.log(artist);
+                //console.log(artist);
                 artistEventsRequests.push(getArtistEvents(artist));
               });
 
               Promise.all(artistEventsRequests).then(artistEventsResponses=>{
-                console.log(artistEventsResponses);
-                console.log(bands);
+                //console.log(artistEventsResponses);
+                //console.log(bands);
               });
             })
             .catch(console.log);
+
         });
 
-      function displayMusicEvents() {
-        
-        var musicDiv = $("<div>");
-        musicDiv.addClass("card-image waves-effect waves-block waves-light");
+      function displayMusicEvents(bandsWithData) {
+        //console.log(bandsWithData);
 
-        var imgURL = results[0].images[0];
+        bandsWithData.forEach(function(eventObject){
+          //console.log(eventObject.name)
+
+      
+       var mainCard = $("<div>");
+       mainCard.addClass("col s6 m4");
+
+       var card = $("<div>");
+       card.addClass("card");
+       mainCard.append(card);
+
+        var cardImage= $("<div>");
+        cardImage.addClass("card-image waves-effect waves-block waves-light");
+        var imgURL = eventObject.image;
         var image = $("<img>").attr("src", imgURL);
-        musicDiv.append(image);
-        console.log(image);
+        image.addClass("activator events-image");
+        //image.append(imgURL); 
+        cardImage.append(image);
+        console.log(imgURL);
+        
+        var cardContent = $("<div>");
+        cardContent.addClass("card-content");
+        
 
-        var artistName = results[0].names;
-        var name = $("<p>").text("Name: " + artistName);
-        musicDiv.append(name);
-        console.log(name);
+        var artistName = eventObject.name;
+        var name = $("<span>").text(artistName);
+        name.addClass("card-title activator grey-text text-darken-4");
+        var iClass = $("<i>").addClass("material-icons right").text("more_vert");
+        name.append(iClass);
+        cardContent.append(name);
+        
 
-        var dateTime = results[0].dates.start[0].localDate.localTime;
-        var date = $("<p>").text("Date and Time: " + dateTime);
-        musicDiv.append(date);
-        console.log(date);
+        var dateTime = eventObject.date + " " + eventObject.time;
+        var date = $("<p>").text("Date & Time: " + dateTime);
+        cardContent.append(date);
+        card.append(cardContent);
+        //console.log(date);
+        
+        var cardReveal = $("<div>");
+        cardReveal.addClass("card-reveal");
+        var cardRevealSpan = $("<span>").addClass("card-title grey-text text-darken-4");
+        var closeBtn = $("<i>").addClass("material-icons right").text("close");
+        cardRevealSpan.text("Event Details");
+        cardRevealSpan.append(closeBtn);
+        cardReveal.append(cardRevealSpan);
 
-        var ticketURL = results[0].ticketLimit.url;
-        var ticket = $("<p>").text(ticketURL);
-        musicDiv.append(ticket);
-        console.log(ticket);
+        var br1 = $("<br>");
+        cardReveal.append(br1);
 
+        var artistName = eventObject.name;
+        var pName = $("<p>").text(artistName);
+        cardReveal.append(pName);
+
+        var dateTime = eventObject.date + " " + eventObject.time;
+        var pDateTime = $("<p>").text(dateTime);
+        cardReveal.append(pDateTime);
+
+        console.log(moment(dateTime))
+
+        var venueName = eventObject.venuename;
+        var pVenueName = $("<p>").text(venueName);
+        cardReveal.append(pVenueName);
+
+
+        var br2 = $("<br>");
+        cardReveal.append(br2);
+        
+        var ticketURL = eventObject.ticketurl;
+        var ticketLink = $("<a>", {href: ticketURL});
+        ticketLink.addClass("waves-effect waves-light btn").text("Buy Ticket");
+        cardReveal.append(ticketLink);
+        card.append(cardReveal);
+        mainCard.append(card);
+
+        $(".results-body").append(mainCard);
+
+      });
 
     };
 
-    $(document).on(displayMusicEvents);
-
-
-
-
-
-
-
-
 
 });
+
+{/*
+
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator" src="assets/images/HipHopGenre.jpg">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">Tech N9ne<i class="material-icons right">more_vert</i></span>
+      <p>Date & Time</p>
+    </div>
+    <div class="card-reveal">
+      <span class="card-title grey-text text-darken-4"><i class="material-icons right">close</i>Event Details</span>
+      <br>
+      <p>Artist Name</p>
+      <p>Date & Time</p>
+      <p>Venue | Address</p>
+      <p>Description</p>
+      <br>
+      <a class="waves-effect waves-light btn">Buy Tickets</a>
+    </div>
+*/}
+
+           
