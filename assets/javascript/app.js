3d3474577d3ab6db174e9d89fd56a58ec66afe49
glosskc <glosskc@gmail.com>
2019-04-11 13:48:56 -0500

    Venue modal popup update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,17 @@
 $(document).ready(function(){
+  $('.modal').modal();
+  $('.modal').modal({
+    dismissible: true, // Modal can be dismissed by clicking outside of the modal
+    opacity: .5, // Opacity of modal background
+    inDuration: 300, // Transition in duration
+    outDuration: 200, // Transition out duration
+    startingTop: '50%', // Starting top style attribute
+    endingTop: '50%', // Ending top style attribute
+    ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
+      alert("Ready");
+      console.log(modal, trigger);
+    },
+  });
     // Location page JS
     $('.slider').slider();
     $('.dropdown-button').dropdown('open');
@@ -16,15 +29,15 @@ $(document).ready(function(){
       }
     );
 
-    $('.datepicker').pickadate({
-        selectMonths: true, // Creates a dropdown to control month
-        selectYears: 15, // Creates a dropdown of 15 years to control year,
-        today: 'Today',
-        clear: 'Clear',
-        close: 'Ok',
-        closeOnSelect: false, // Close upon selecting a date,
-        container: undefined, // ex. 'body' will append picker to body
-      });
+    //$('.datepicker').pickadate({
+       // selectMonths: true, // Creates a dropdown to control month
+        //selectYears: 15, // Creates a dropdown of 15 years to control year,
+        //today: 'Today',
+        //clear: 'Clear',
+        //close: 'Ok',
+        //closeOnSelect: false, // Close upon selecting a date,
+        //container: undefined, // ex. 'body' will append picker to body
+      //});
   
 
 
