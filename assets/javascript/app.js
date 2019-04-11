e0b4ae83146fe53a1bc32a3b745986c6d6d1aa66
CKayV11 <11303975+ckayv11@users.noreply.github.com>
2019-04-10 20:07:26 -0500

    stylized every file by adding cards, tables, href, css

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,42 +1,9 @@
 $(document).ready(function(){
     // Location page JS
     $('.slider').slider();
-    $('.carousel.carousel-slider').carousel({fullWidth: true});
-    // $('.carousel').carousel({fullWidth: true});
-    // $('.dropdown-button').dropdown('open');
-    // $('.dropdown-button').dropdown('close');
-    
-    // $('.dropdown-button').dropdown({
-    //     inDuration: 300,
-    //     outDuration: 225,
-    //     constrainWidth: false, // Does not change width of dropdown to that of the activator
-    //     hover: true, // Activate on hover
-    //     gutter: 0, // Spacing from edge
-    //     belowOrigin: false, // Displays dropdown below the button
-    //     alignment: 'left', // Displays dropdown with edge aligned to the left of button
-    //     stopPropagation: false // Stops event propagation
-    //   }
-    // );
 
-    // $('.datepicker').pickadate({
-    //     selectMonths: true, // Creates a dropdown to control month
-    //     selectYears: 15, // Creates a dropdown of 15 years to control year,
-    //     today: 'Today',
-    //     clear: 'Clear',
-    //     close: 'Ok',
-    //     closeOnSelect: false, // Close upon selecting a date,
-    //     container: undefined, // ex. 'body' will append picker to body
-    //   });
 
-  //   $('.datepicker').pickadate({
-  //       selectMonths: true, // Creates a dropdown to control month
-  //       selectYears: 15, // Creates a dropdown of 15 years to control year,
-  //       today: 'Today',
-  //       clear: 'Clear',
-  //       close: 'Ok',
-  //       closeOnSelect: false, // Close upon selecting a date,
-  //       container: undefined, // ex. 'body' will append picker to body
-  //     });
+
 
   var bands = {};
 
